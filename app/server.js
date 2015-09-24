b45c6c0221b5c3eaccc818415d33ae44383754dc
lirlia <lilria5000@gmail.com>
2015-09-24 23:21:53 +0900

    add

--- app/server.js
+++ app/server.js
@@ -1,9 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
 	function onRequest(request,response) {
-	        console.log("request received.");
-	        response.writeHead(404,{"Content-Type":"text/plain"});
+		var pathname = url.parse(request.url).pathname;
+	        console.log("request for " + pathname + " received.");
+	        
+		route(pathname);
+
+		response.writeHead(404,{"Content-Type":"text/plain"});
 	        response.write("Hello World");
 	        response.end();
 	}
